e5ea8c1eb315c59d4f2fd374c294125e991839c7
Devansh Patel <devansh.dp0403@gmail.com>
2024-09-25 19:06:39 +0530

    Delete InputValidator.java

--- InputValidator.java
+++
@@ -1,9 +0,0 @@
-public class InputValidator {
-    public void validateInput(String input) throws CustomException {
-        if (input == null || input.trim().isEmpty()) {
-            throw new CustomException("Input cannot be empty or null.");
-        }
-        
-        // Add more validation rules as required
-    }
-}
